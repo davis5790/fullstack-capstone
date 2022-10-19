4660343bc6a7fa2894c1cc00a04a41b5dd504b34
Chris Hornick <christopher.hornick@coachusa.com>
2022-10-19 15:23:29 -0400

    s3 bucket

--- lib/cdk-workshop-stack.ts
+++ lib/cdk-workshop-stack.ts
@@ -2,13 +2,18 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as apigw from 'aws-cdk-lib/aws-apigateway';
+import { Bucket, BucketAccessControl} from 'aws-cdk-lib/aws-s3';
 
 export class CdkWorkshopStack extends cdk.Stack {
   public readonly hcViewerUrl: cdk.CfnOutput;
   public readonly hcEndpoint: cdk.CfnOutput;
-
+  
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+     super(scope, id, props);
+     const bucket = new Bucket(this, 'Bucket', {
+       accessControl: BucketAccessControl.PRIVATE,
+     })
+    
 
     const hello = new lambda.Function(this, 'HelloHandler', {
       runtime: lambda.Runtime.NODEJS_14_X,
@@ -17,3 +22,5 @@ export class CdkWorkshopStack extends cdk.Stack {
     });
   }
 }
+
+
