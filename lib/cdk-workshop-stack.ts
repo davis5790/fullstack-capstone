078f2c3d4cc8e558a319d61db3bc535af5b8353b
Chris Hornick <christopher.hornick@coachusa.com>
2022-11-03 20:25:22 -0400

    fargate service added again and code clean up

--- lib/cdk-workshop-stack.ts
+++ lib/cdk-workshop-stack.ts
@@ -16,75 +16,16 @@ export class CdkWorkshopStack extends cdk.Stack {
 
     constructor(scope: Construct, id: string, env: string, frontendProps: FrontendProps) {
         super(scope, id, frontendProps);
-        // const userData = ec2.UserData.forLinux()
-        // const webBuild = new Asset(this, `web-build-${env}`, {
-        //     path: 'test-app'
-        // });
-        // userData.addCommands("sudo apt install awscli -y")
-        // userData.addCommands("sudo apt install python3-pip -y")
-        // userData.addCommands("pip install flask")
-
-        // userData.addS3DownloadCommand({
-        //     bucket: webBuild.bucket,
-        //     bucketKey: webBuild.s3ObjectKey,
-        //     localFile: "/home/ubuntu/helloworld"
-        // })
-
-        // userData.addCommands("sudo apt install unzip -y")
-        // userData.addCommands(`unzip -o /home/ubuntu/helloworld/${webBuild.s3ObjectKey}`)
-        // userData.addCommands('sudo systemctl restart helloworld')
+      
         const vpc = new ec2.Vpc(this, 'clout-vpc', {
         }) //creates a VPC
 
-        // const privateSubnets = vpc.selectSubnets({
-        //     subnetType: ec2.SubnetType.PRIVATE_WITH_NAT
-        // });
-
-        // const publicSubnets = vpc.selectSubnets({
-        //     subnetType: ec2.SubnetType.PUBLIC
-        // });
-
-        // const cloutComputingAsg = new autoscaling.AutoScalingGroup(this, "cloutfrontend-asg", {
-        //     vpc: vpc,
-        //     instanceType: frontendProps.autoscaling.instanceType,
-        //     machineImage: new ec2.GenericLinuxImage({ "us-east-1": "ami-0d837fa72073ab772" },{
-        //         userData: userData
-        //     }), ///nick's ec2
-        //     minCapacity: frontendProps.autoscaling.minCapacity,
-        //     maxCapacity: frontendProps.autoscaling.maxCapacity,
-        //     vpcSubnets: publicSubnets,
-        //     associatePublicIpAddress: true,
-        //     keyName: `clout-bastion-${env}`,
-        //     updatePolicy: autoscaling.UpdatePolicy.replacingUpdate(),
-        //     allowAllOutbound: true,
-        // });
-
-        // webBuild.bucket.grantRead(cloutComputingAsg);
-
-        // const cloutFrontendAlb = new alb.ApplicationLoadBalancer(this, "cloutfrontend-alb", {
-        //     vpc: vpc,
-        //     internetFacing: true,
-        //     vpcSubnets: publicSubnets,
-        // })
-
-        // const listener = cloutFrontendAlb.addListener('cloutfrontend-listener', {
-        //     port: 80
-        // })
-
-        // listener.addTargets("cloutfrontend-targets", {
-        //     port: 80,
-        //     targets: [cloutComputingAsg],
-        //     // healthCheck: {
-        //     //     enabled: true,
-        //     //     healthyHttpCodes: "200"
-        //     // },
-        // })
-
+        
         const cluster = new ecs.Cluster(this, "clout-cluster", {
             enableFargateCapacityProviders: true,
             vpc: vpc
         });
-        new ecs_patterns.ApplicationLoadBalancedEc2Service(this, 'clout-service', {
+        new ecs_patterns.ApplicationLoadBalancedFargateService(this, 'clout-service', {
             cluster: cluster,
             memoryLimitMiB: 512,
             desiredCount: frontendProps.autoscaling.maxCapacity,
